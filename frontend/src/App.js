f72b2958b699a62091e57744e31bb8516a8d1293
Vithursan Magenthirarajah <57103371+vithursanmagenthirarajah@users.noreply.github.com>
2021-08-11 15:11:03 +0530

    SPM project setup finished

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+
+
+//import
+//import Sample from "./Vithursan/sample.jsx"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className="App">
+    <Router>
+      <div className="container">
+        <Switch> 
+            <Route path='/' exact={true} component={}/>
+        </Switch>
+      </div>
+    </Router>
+  </div>
   );
 }
 
 export default App;
+ 
