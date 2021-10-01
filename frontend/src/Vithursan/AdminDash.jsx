f85c55233a9168095e71306db924d616079a9cc5
Vithursan Magenthirarajah <57103371+vithursanmagenthirarajah@users.noreply.github.com>
2021-10-01 23:12:44 +0530

    search order payment completed

--- frontend/src/Vithursan/AdminDash.jsx
+++ frontend/src/Vithursan/AdminDash.jsx
@@ -7,6 +7,7 @@ import Cardi from '../Vithursan/Images/card.jpg'
 import { Link } from "react-router-dom";
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import orderI from '../Vithursan/Images/exp.jpg'
 export default class AdminDash extends Component {
     render() {
         return (
@@ -14,7 +15,7 @@ export default class AdminDash extends Component {
             <br/><br/><br/>
             <h1 className="text-center" style={{color:"#0e7794"}}>Food Management Dashboard</h1>
             <br/><br/><br/>
-            <Row xs={1} md={3} className="g-4">
+            <Row xs={1} md={2} className="g-4">
             <Col>
            
             <Card style={{ width: '20rem' }} className="text-center">
@@ -59,6 +60,22 @@ export default class AdminDash extends Component {
             <Link to="/card"><Button variant="#053b4b" size="lg" block   style={{backgroundColor:"#053b4b", color:"white"}}>click</Button></Link>
           </Card.Body>
         </Card>
+        </Col>
+
+        <Col>
+        <Card style={{ width: '18rem' }} className="text-center">
+        <Link to ="/vieworder">
+          <Card.Img variant="top" src={orderI} />
+</Link>
+          <Card.Body>
+            <Card.Title>Order Details </Card.Title>
+            <Card.Text>
+             
+            </Card.Text>
+            <Link to="/vieworder"><Button variant="#053b4b" size="lg" block   style={{backgroundColor:"#053b4b", color:"white"}}>click</Button></Link>
+          </Card.Body>
+        </Card>
+       
         </Col>
         </Row>
             </div>
